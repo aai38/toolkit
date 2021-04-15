71eb7720cb7ee47e1d0ef8f2097e88f470fd6dc0
Karolin Bartlmae <karolin.bartlmae@uni-ulm.de>
2021-04-15 17:22:08 +0200

    changed line chart and card component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -2,6 +2,7 @@ import { NgModule, Component, Input, Output, EventEmitter } from '@angular/core'
 
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import ButtonComponent from '../button/button.component';
 
 
 
@@ -16,6 +17,14 @@ export default class CardComponent  {
   constructor() { }
   @Input()
   backgroundColor?: string;
+  @Input()
+  image?: string;
+  @Input()
+  width?: number;
+  @Input()
+  height?: number;
+  @Input()
+  button?: ButtonComponent;
 
 
 }
